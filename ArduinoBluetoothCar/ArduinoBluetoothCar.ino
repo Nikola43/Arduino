e83c2bc788cc8dca6d90d756f31d2d42742c2f7a
Paulo Soares <pauloxti@gmail.com>
2017-01-31 00:18:50 +0100

    tone

--- ArduinoBluetoothCar/ArduinoBluetoothCar.ino
+++ ArduinoBluetoothCar/ArduinoBluetoothCar.ino
@@ -1,3 +1,4 @@
+#include "notas.h"
 #include "ArduinoCar.h"
 #include <SoftwareSerial.h>
 #define TXD 4
@@ -6,11 +7,39 @@
 SoftwareSerial BTSerial(RXD, TXD); // RX | TX
 ArduinoCar * coche = new ArduinoCar();
 
+// notes in the melody:
+int melody[] = {
+  NOTE_C4, NOTE_G3,NOTE_G3, NOTE_A3, NOTE_G3,0, NOTE_B3, NOTE_C4};
+
+// note durations: 4 = quarter note, 8 = eighth note, etc.:
+int noteDurations[] = {
+  4, 8, 8, 4,4,4,4,4 };
+  
+  
 char datosRecibidos = '0';
 char datosEnviados = '0';
+boolean estaAndando = false;
+
 
 void setup()
 {
+  // iterate over the notes of the melody:
+  for (int thisNote = 0; thisNote < 8; thisNote++) {
+
+    // to calculate the note duration, take one second 
+    // divided by the note type.
+    //e.g. quarter note = 1000 / 4, eighth note = 1000/8, etc.
+    int noteDuration = 1000/noteDurations[thisNote];
+    tone(6, melody[thisNote],noteDuration);
+
+    // to distinguish the notes, set a minimum time between them.
+    // the note's duration + 30% seems to work well:
+    int pauseBetweenNotes = noteDuration * 1.30;
+    delay(pauseBetweenNotes);
+    // stop the tone playing:
+    noTone(6);
+  }
+    
 	Serial.begin(115200);
 	BTSerial.begin(115200);
 }
@@ -36,20 +65,30 @@ void loop()
 	}
 	
 	if ( datosRecibidos != 0 )
-	{
-		if ( datosRecibidos == '1' )
-		{
-			coche->marchaAdelante()	;
-		}
-		if ( datosRecibidos == '3' )
-		{
-			coche->marchaAtras();
-		}
-		if ( datosRecibidos == '2' )
-		{
-      
-			coche->parar();
-		}
+	{   
+            switch(datosRecibidos)
+            {
+                case '1'  : coche->marchaAdelante(); break;
+
+                
+                /*
+                case '2'  : coche->coche->parar();   break;
+                case '3'  : coche->marchaAtras();    break;
+                case '4'  : coche->coche->parar();   break;
+                case '5'  : coche->coche->parar();   break;
+                case '6'  : coche->coche->parar();   break;
+                case '7'  : coche->coche->parar();   break;
+                case '8'  : coche->coche->parar();   break;
+                case '9'  : coche->coche->parar();   break;
+                case '10' : coche->coche->parar();   break;
+                case '11' : coche->coche->parar();   break;
+                case '12' : coche->coche->parar();   break;
+                case '13' : coche->coche->parar();   break;
+                case '14' : coche->coche->parar();   break;
+                case '15' : coche->coche->parar();   break;
+                */
+                
+            }
 	}
 
   delay(1);
